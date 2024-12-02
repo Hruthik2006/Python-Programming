8864d9d0a760cb1a58821230257fc8ae363ecc52
Hruthik2006 <84223859+Hruthik2006@users.noreply.github.com>
2024-12-02 18:10:06 +0530

    Create FrequencyOfDigits (P4).py
    Program to find the frequency of digits.

---
+++ FrequencyOfDigits (P4).py
@@ -0,0 +1,22 @@
+# Problem Statement
+# Read a multi-digit number (as chars) from the console. 
+# Develop a program to print the frequency of each digit with suitable message.
+
+def calculate_digit_frequency (number):
+    frequency = {}
+    for digit in number:
+    if digit.isdigit():
+        frequency [digit] = frequency.get(digit, 0) + 1
+    return frequency
+
+def display_digit_frequency(frequency):
+    print("\nFrequency of each digit:")
+    for digit, count in sorted(frequency.items()):
+        print(f"Digit (digit}: {count} time(s)")
+
+number input("Enter a multi-digit number: ")
+
+frequency = calculate_digit_frequency (number)
+
+display_digit_frequency (frequency)
+
