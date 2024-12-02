d1caae5eca36b4fb0cf9f7c8cd7fa75e00783255
Hruthik2006 <84223859+Hruthik2006@users.noreply.github.com>
2024-12-02 18:28:39 +0530

    Create P7 SortingTheContents.py
    Program to sort the contents of a text file and write the sorted contents into a separate text file.

---
+++ P7 SortingTheContents.py
@@ -0,0 +1,28 @@
+# Problem Statement
+# Develop a program to sort the contents of a text file and write the sorted contents into a separate text file.
+
+
+def sort_file_contents(input_file_path, output_file_path):
+    try:
+        with open(input_file_path, 'r') as file:
+            lines file.readlines()
+        
+        stripped_lines = [line.strip() for line in lines]
+        stripped_lines.sort()
+
+        for line in stripped_lines:
+            file.write(line + '\n')
+            print(f"Sorted contents have been written to 'output_file_path}'.")
+            
+    except FileNotFoundError:
+            print(f"The file '{input_file_path}' was not found.")
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+input_file_path = input("Enter the path to the input text file: ")
+
+output_file_path = input("Enter the path to the output text file: ")
+
+sort_file_contents (input_file_path, output_file_path)
+
