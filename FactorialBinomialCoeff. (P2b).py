9143d452987d549c3c7fa5fd06609330c92dd1d9
Hruthik2006 <84223859+Hruthik2006@users.noreply.github.com>
2024-12-02 18:04:17 +0530

    Create FactorialBinomialCoeff. (P2b).py
    Program to calculate factorial and to compute binomial coefficient.

---
+++ FactorialBinomialCoeff. (P2b).py
@@ -0,0 +1,21 @@
+# Problem Statement
+# Write a function to calculate factorial of a number. 
+# Develop a program to compute binomial coefficient (Given N and R).
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def binomial_coefficient(n, r):
+    return factorial(n) // (factorial(r) * factorial(n- r))
+
+r=int(input("Enter the value of R: "))
+
+if r > n:
+    print("Invalid input: R cannot be greater than N.")
+else:
+    result = binomial_coefficient(n, r)
+    print(f"\nBinomial coefficient c({n}, {r}) = {result}")
+
